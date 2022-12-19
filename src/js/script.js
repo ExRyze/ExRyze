ed295a35237a56a7599399d9cf07fb9a1b7ae80e
ExRyze <vaisyagovinandas@gmail.com>
2022-12-19 21:08:46 +0800

    Update - Init - NPM, Webpack, Contents (Navbar - Header - half About)

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+import logo_ex from '../img/logo_ex.png';
+import './../css/style.css';
+import 'jquery';
+import 'bootstrap';
---
+++ webpack.common.js
@@ -0,0 +1,62 @@
+const path = require('path');
+// const ESLintPlugin = require('eslint-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    index: ['bootstrap/dist/css/bootstrap.min.css', './src/js/script.js']
+  },
+  output: {
+    filename: 'script.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+ optimization: {
+   splitChunks: {
+     chunks: 'all',
+   },
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    // new ESLintPlugin(),
+    new MiniCSSExtractPlugin({
+      filename: 'style.[contenthash].css'
+    })
+  ],
+  devServer: {
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCSSExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(jpg|gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
