5c95ddee0be498864e1d274f434a82ac9ec9bf90
ExRyze <vaisyagovinandas@gmail.com>
2023-06-26 16:45:15 +0800

    Generate Structure

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,9 @@
-// var $ = require('jquery');
+const $ = require('jquery');
 require('bootstrap');
+
+// Fixed Footer
+const footer = $('.footer');
+const bodyH = $('body').height();
+if (footer && (bodyH - footer.height()) <= footer.offset().top) {
+  footer.addClass('fixed');
+}
