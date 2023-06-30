fc7368464b03cd4826b0c5d9775e76729543f76f
ExRyze <vaisyagovinandas@gmail.com>
2023-06-30 13:26:32 +0800

    Create - Navbar

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,2 @@
 const $ = require('jquery');
 require('bootstrap');
-
-// Fixed Footer
-const footer = $('.footer');
-const bodyH = $('body').height();
-if (footer && (bodyH - footer.height()) <= footer.offset().top) {
-  footer.addClass('fixed');
-}
